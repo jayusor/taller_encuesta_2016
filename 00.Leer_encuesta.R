20dd9aa7531c280e651a1908a6863870283028c8
Jorge Ayuso <jayusor@gmail.com>
2016-10-09 13:38:13 +0200

    Ejemplo lectura hoja encuesta

--- 00.Leer_encuesta.R
+++ 00.Leer_encuesta.R
@@ -1,15 +1,23 @@
 rm(list = ls());gc()
 
-
 library(tidyverse)
 library(googlesheets)
 
+## Buscar por hojas que contengan "Europea"
+(hojas <- gs_ls("Europea")) %>% str
 
-quiero <- "Taller Europea (respuestas)"
+## Guardo la key de la hoja
+key_hoja <- hojas %>% select(sheet_key) %>% .[[1]]
 
+## Defino `wb` que hace referencia a la hoja que queremos
+wb <- gs_key(key_hoja)
+wb
 
-gs_ls() %>% filter(sheet_title==quiero) %>% str
+## Abrir en el navegador
+# wb %>% gs_browse()
 
+## Leer la hoja 
+meta <- wb %>% gs_read()
+class(meta)
 
-
-
+meta
