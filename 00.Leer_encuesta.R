d5ab7487f0566c53b2fb93b204911beb572a07c7
Jorge Ayuso <jayusor@gmail.com>
2016-10-09 14:28:12 +0200

    notebook rmarkdown

--- 00.Leer_encuesta.R
+++ 00.Leer_encuesta.R
@@ -1,22 +1,41 @@
+#' ---
+#' title: "Lectura Encuesta"
+#' author: Lenguajes para el Análisis de Datos
+#' date: "`r format(Sys.time(), '%d de %B, %Y')`"
+#' mode: standalone # {selfcontained,standalone, draft}
+#' output:
+#'  html_document:
+#'    theme: cerulean
+#'    css: ./img/base.css
+#' ---
+
+
+#' ## Introducción
+#' 
+#' Vamos a proceder a leer los resultados de la encuesta de Google Forms
+#' 
+
+
 rm(list = ls());gc()
 
 library(tidyverse)
 library(googlesheets)
 
-## Buscar por hojas que contengan "Europea"
+#' Buscar por hojas que contengan "Europea"
 (hojas <- gs_ls("Europea")) %>% str
 
-## Guardo la key de la hoja
+#' Guardo la key de la hoja
 key_hoja <- hojas %>% select(sheet_key) %>% .[[1]]
 
-## Defino `wb` que hace referencia a la hoja que queremos
+#' Defino `wb` que hace referencia a la hoja que queremos
 wb <- gs_key(key_hoja)
 wb
 
-## Abrir en el navegador
-# wb %>% gs_browse()
+#' Abrir en el navegador
+#+ eval=FALSE
+wb %>% gs_browse()
 
-## Leer la hoja 
+#' Leer la hoja 
 meta <- wb %>% gs_read()
 class(meta)
 
