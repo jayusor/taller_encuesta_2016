fa75739abac676ef09f50a07d0637c9798ae8008
@aschinchon <aschinchon@gmail.com>
2016-10-11 21:02:47 +0200

    Estadísticas básicas, primeros plots y creción e variables

--- 00.Leer_encuesta.R
+++ 00.Leer_encuesta.R
@@ -39,5 +39,48 @@ wb %>% gs_browse()
 meta <- wb %>% gs_read()
 class(meta)
 
-#' Empieza mi parte
-meta
+#' ## Algunas operaciones sencillas con los datos
+#' Sacar unos cuantos registros
+head(meta)
+str(meta)
+
+#' Renombrar campos para facilitar manejo
+colnames(meta) 
+colnames(meta)<-c("date", "estado", "biblioteca", "accesibilidad_parking", "comidas", 
+                  "inst_aulas", "inst_deporte", "accesibilidad_profes", "seguridad", 
+                  "extraescolares", "residencias", "satisfaccion", "recomendacion",
+                  "nombre", "domicilio", "email")
+str(meta)
+
+#' Seleccionar los que dicen que el estado del campus es 3 o menos
+meta %>% filter(estado<=3)
+
+#' Summary del dataset
+summary(meta)
+
+#' Histograma de una respuesta
+hist(meta$comidas, breaks=0:10, main = "Valoracion de las comidas")
+
+#' x-y plot
+plot(x = meta$inst_deporte, 
+     y = meta$inst_aulas, 
+     type = 'p', 
+     main = "Valoracion de las instalaciones")
+
+#' ## Creamos algunas variables nuevas
+#' Promotor/Detractor
+meta %>% mutate(nps=ifelse(recomendacion<3, "Detractor", ifelse(recomendacion>7, "Promotor", "Neutro")))
+
+#' Genero (M/F)
+library(genderizeR)
+meta %>% 
+  select(nombre) %>% 
+  findGivenNames() -> sexo
+meta %>% left_join(sexo, by=c("nombre"="name")) -> meta
+
+#' Geolocalizacion
+library(ggmap)
+meta %>% 
+  select(domicilio) %>% 
+  lapply(geocode)->loc
+meta %>% cbind(loc)->meta
